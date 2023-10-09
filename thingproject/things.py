5e935499ffafba1cd6fcaef688e405095f0cff00
Bruno Miguel Fonseca Cavaca (k22017608) <bruno.fonseca_cavaca@kcl.ac.uk>
2023-10-09 15:47:40 +0100

    TestPush1

---
+++ thingproject/things.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.db.models import Model
+
+class Thing(Model):
+    name =models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
